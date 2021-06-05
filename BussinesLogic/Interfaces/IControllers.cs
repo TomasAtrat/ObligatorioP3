6a31051d0b9756fb8de7130090a257dff23871df
tomasAtrat <tomasatrat@gmail.com>
2021-06-05 14:36:50 -0300

    Interface for Controllers

---
+++ BussinesLogic/Interfaces/IControllers.cs
@@ -0,0 +1,20 @@
+﻿using CommonSolution.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Interfaces
+{
+    public interface IControllers
+    {
+        List<string> Alta(IDto dto);
+
+        List<string> Baja(IDto dto);
+
+        List<string> Modificacion(IDto dto);
+
+        List<IDto> Listado();
+    }
+}
