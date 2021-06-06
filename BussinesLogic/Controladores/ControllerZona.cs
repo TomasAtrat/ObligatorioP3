c87d030f0a7583113a29ac4ccfc2a9d41e38d43f
tomasAtrat <tomasatrat@gmail.com>
2021-06-06 14:30:53 -0300

    Implementación interfaz para el controlador de zona

---
+++ BussinesLogic/Controladores/ControllerZona.cs
@@ -0,0 +1,43 @@
+﻿using BussinesLogic.Interfaces;
+using CommonSolution.Dto;
+using CommonSolution.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Controladores
+{
+    class ControllerZona : IControllers
+    {
+        public List<string> Alta(IDto dto)
+        {
+            List<string> colErrores = this.Validate((DtoZona)dto);
+
+            return colErrores;
+        }
+
+        public List<string> Baja(IDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IDto> Listado()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> Modificacion(IDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<string> Validate(DtoZona dto)
+        {
+            List<string> colErrores = new List<string>();
+
+            return colErrores;
+        }
+    }
+}
