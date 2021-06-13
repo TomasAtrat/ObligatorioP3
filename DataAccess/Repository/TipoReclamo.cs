78bca801e77eb12a1b8f1f06b9829640ec20ff91
jose barrera <josebarrera16@outlook.com>
2021-06-13 18:50:32 -0300

    PROBANDO

---
+++ DataAccess/Repository/TipoReclamo.cs
@@ -0,0 +1,35 @@
+﻿using CommonSolution.Dto;
+using DataAccess.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model;
+
+namespace DataAccess.Repository
+{
+    public class TipoReclamo
+    {
+
+        public TipoReclamo()
+        {
+            this.TipoReclamoMapper = new TipoReclamoMapper();
+        }
+
+        private TipoReclamoMapper TipoReclamoMapper;
+
+        public void AddReclamo(DtoTipoReclamo dto)
+        {
+            using (Context contex = new Context())
+            { 
+            
+            
+            }
+        }
+
+
+    }
+}
