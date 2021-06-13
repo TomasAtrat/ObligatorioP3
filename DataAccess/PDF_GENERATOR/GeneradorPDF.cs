3634df6734e3cc8704bee2aecc207441ad8461bf
tomasAtrat <tomasatrat@gmail.com>
2021-06-12 23:08:52 -0300

    ReclamoRepository

---
+++ DataAccess/PDF_GENERATOR/GeneradorPDF.cs
@@ -0,0 +1,27 @@
+﻿using CommonSolution.Dto;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.PDF_GENERATOR
+{
+    public class GeneradorPDF
+    {
+        public GeneradorPDF()
+        {
+            this.doc = new Document();
+        }
+
+        private Document doc;
+        private PdfWriter writer;
+
+        public void ReporteReclamo(DtoReclamo reclamo, string folder)
+        {
+
+        }
+    }
+}
