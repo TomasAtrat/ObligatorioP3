4f909d6d64a8480574b55343e5f68c0a5975ef44
tomasAtrat <tomasatrat@gmail.com>
2021-06-14 18:51:50 -0300

    Reporte de reclamos asignados por cuadrilla

---
+++ BussinesLogic/Controladores/ControllerReporte.cs
@@ -0,0 +1,29 @@
+﻿using BussinesLogic.Interfaces;
+using CommonSolution.Dto;
+using DataAccess.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Controladores
+{
+    public class ControllerReporte
+    {
+        public ControllerReporte()
+        {
+            this.repository = new Repository();
+        }
+
+        private Repository repository;
+
+        public List<DtoReclamo> getReclamosAsignadosPorCuadrilla(long idCuadrilla)
+        {
+            List<DtoReclamo> colDtos = this.repository.ReclamoRepository.getElements();
+            return colDtos.Where(i => i.IDCuadrilla == idCuadrilla && (i.Estado.ToString() == "ASIGNADO" || i.Estado.ToString() == "EN_PROCESO")).ToList();
+        }
+
+        
+    }
+}
