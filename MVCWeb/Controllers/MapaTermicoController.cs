01d477190b2b0d4b38c32b303f85fe90e23c22dc
jose barrera <josebarrera16@outlook.com>
2021-07-04 01:21:38 -0300

    agregandoMapaTermico

---
+++ MVCWeb/Controllers/MapaTermicoController.cs
@@ -0,0 +1,24 @@
+﻿using BussinesLogic.Controladores;
+using BussinesLogic.Interfaces;
+using CommonSolution.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWeb.Controllers
+{
+    public class MapaTermicoController : Controller
+    {
+        
+        public ActionResult Mapatermico()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
