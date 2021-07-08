9282bb09240af686f31e6d2b627f91f190e16d1d
tomasAtrat <tomasatrat@gmail.com>
2021-07-07 22:16:58 -0300

    .

---
+++ MVCWeb/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using BussinesLogic.Controladores;
+using CommonSolution.Dto;
+using CommonSolution.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated == true)
+            {
+                return Redirect("/Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(DtoLogin dto)
+        {
+            LControllerLogin login = new LControllerLogin();
+            DtoUsuario usuario = login.Registrado(dto.NombreUsuario, dto.Password);
+            if (usuario.NombreUsuario == dto.NombreUsuario && usuario.Password == dto.Password && dto != null)
+            {
+
+                FormsAuthentication.SetAuthCookie(dto.NombreUsuario, true);
+
+
+                return Redirect("/Home");
+            }
+
+            return View();
+
+        }
+        public ActionResult LogOut()
+        {
+            //SignOut() Limpia la Cookie de Autenticación
+            FormsAuthentication.SignOut();
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
